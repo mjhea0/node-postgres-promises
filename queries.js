a501efa89c571eae353d0f74fa20a6fd7ea3111c
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2016-03-23 08:20:18 +0000

    Update queries.js
    Adding integer casting everywhere where it is required.

--- queries.js
+++ queries.js
@@ -1,8 +1,8 @@
 var promise = require('bluebird');
 
 var options = {
-  // Initialization Options
-  promiseLib: promise
+    // Initialization Options
+    promiseLib: promise
 };
 
 var pgp = require('pg-promise')(options);
@@ -10,90 +10,91 @@ var connectionString = 'postgres://localhost:5432/puppies';
 var db = pgp(connectionString);
 
 function getAllPuppies(req, res, next) {
-  db.any('select * from pups')
-  .then(function(data) {
-    res.status(200)
-      .json({
-        status: 'success',
-        data: data,
-        message: 'Retrieved ALL puppies'
-      });
-  })
-  .catch(function(err) {
-    return next(err);
-  });
+    db.any('select * from pups')
+        .then(function (data) {
+            res.status(200)
+                .json({
+                    status: 'success',
+                    data: data,
+                    message: 'Retrieved ALL puppies'
+                });
+        })
+        .catch(function (err) {
+            return next(err);
+        });
 }
 
 function getSinglePuppy(req, res, next) {
-  var pupID = req.params.id;
-  db.one('select * from pups where id = $1', pupID)
-    .then(function(data) {
-      res.status(200)
-        .json({
-          status: 'success',
-          data: data,
-          message: 'Retrieved ONE puppy'
+    var pupID = parseInt(req.params.id);
+    db.one('select * from pups where id = $1', pupID)
+        .then(function (data) {
+            res.status(200)
+                .json({
+                    status: 'success',
+                    data: data,
+                    message: 'Retrieved ONE puppy'
+                });
+        })
+        .catch(function (err) {
+            return next(err);
         });
-    })
-    .catch(function(err) {
-      return next(err);
-    });
 }
 
 function createPuppy(req, res, next) {
-  db.none('insert into pups(name, breed, age, sex)'+
-          'values(${name}, ${breed}, ${age}, ${sex})',
-          req.body)
-    .then(function() {
-      res.status(200)
-        .json({
-          status: 'success',
-          message: 'Inserted one puppy'
+    req.body.age = parseInt(req.body.age);
+    db.none('insert into pups(name, breed, age, sex)' +
+            'values(${name}, ${breed}, ${age}, ${sex})',
+        req.body)
+        .then(function () {
+            res.status(200)
+                .json({
+                    status: 'success',
+                    message: 'Inserted one puppy'
+                });
+        })
+        .catch(function (err) {
+            return next(err);
         });
-    })
-    .catch(function(err) {
-      return next(err);
-    });
 }
 
 function updatePuppy(req, res, next) {
-  db.none('update pups set name=$1, breed=$2, age=$3, sex=$4 where id=$5',
-           [req.body.name, req.body.breed, req.body.age,
-            req.body.sex, req.params.id])
-    .then(function() {
-      res.status(200)
-        .json({
-          status: 'success',
-          message: 'Updated puppy'
+    db.none('update pups set name=$1, breed=$2, age=$3, sex=$4 where id=$5',
+        [req.body.name, req.body.breed, parseInt(req.body.age),
+            req.body.sex, parseInt(req.params.id)])
+        .then(function () {
+            res.status(200)
+                .json({
+                    status: 'success',
+                    message: 'Updated puppy'
+                });
+        })
+        .catch(function (err) {
+            return next(err);
         });
-    })
-    .catch(function(err) {
-      return next(err);
-    });
 }
 
 function removePuppy(req, res, next) {
-  var pupID = req.params.id;
-  db.result('delete from pups where id = $1', pupID)
-    .then(function(result) {
-      /* jshint ignore:start */
-      res.status(200)
-        .json({
-          status: 'success',
-          message: `Removed ${result.rowCount} puppy`
+    var pupID = parseInt(req.params.id);
+    db.result('delete from pups where id = $1', pupID)
+        .then(function (result) {
+            /* jshint ignore:start */
+            res.status(200)
+                .json({
+                    status: 'success',
+                    message: `Removed ${result.rowCount} puppy`
+                });
+            /* jshint ignore:end */
+        })
+        .catch(function (err) {
+            return next(err);
         });
-      /* jshint ignore:end */
-    })
-    .catch(function(err) {
-      return next(err);
-    });
 }
 
 
 module.exports = {
-  getAllPuppies: getAllPuppies,
-  getSinglePuppy: getSinglePuppy,
-  createPuppy: createPuppy,
-  updatePuppy: updatePuppy,
-  removePuppy: removePuppy
+    getAllPuppies: getAllPuppies,
+    getSinglePuppy: getSinglePuppy,
+    createPuppy: createPuppy,
+    updatePuppy: updatePuppy,
+    removePuppy: removePuppy
 };
