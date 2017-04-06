2f5329cf57984e300851cde705efc73457205f85
KERTZ <CRKERT@monsanto.com>
2017-04-06 16:38:14 -0500

    reverted to --dirname ref

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var routes = require('./routes/index');
 var app = express();
 
 // view engine setup
-app.set('views', path.join('./public/views'));
+app.set('views', path.join(__dirname, 'public/views'));
 app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
@@ -19,7 +19,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 
