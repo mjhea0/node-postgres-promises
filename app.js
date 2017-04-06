5ecd1390db04b3713c2b842f48bd31df2c6ab077
KERTZ <CRKERT@monsanto.com>
2017-04-06 16:34:02 -0500

    fixed error not allowing views to display

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var pug = require('pug');
 var routes = require('./routes/index');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join('./public/views'));
 app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
@@ -19,7 +19,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.use('/', routes);
 
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-
 var db = require('../queries');
 
 
@@ -10,5 +9,10 @@ router.post('/api/puppies', db.createPuppy);
 router.put('/api/puppies/:id', db.updatePuppy);
 router.delete('/api/puppies/:id', db.removePuppy);
 
+// application -------------------------------------------------------------
+router.get('/', function (req, res) {
+
+    res.render('index', {title: 'node-postgres-promises'}); // load the single view file (angular will handle the page changes on the front-end)
+});
 
 module.exports = router;
